fa0e977514960e7b75ef26b09703391c656526e9
Park-Sin-Ae <qkrtlsdo92@gmail.com>
2022-12-22 07:05:57 +0900

    개인사이트 css 수정

--- js/main.js
+++ js/main.js
@@ -1,7 +1,4 @@
-
-$(function(){
-	$('.gr_ps').animate({width: '95%'}, 2000);
-	$('.gr_html').animate({width: '95%'}, 1500);
-	$('.gr_jquery').animate({width: '60%'}, 2000);
-	$('.gr_illu').animate({width: '80%'},1500);
-});
+		document.querySelector('.gr_ps').animate({width: '95%'}, 2000);
+		document.querySelector('.gr_html').animate({width: '95%'}, 1500);
+		document.querySelector('.gr_jquery').animate({width: '60%'}, 2000);
+		document.querySelector('.gr_illu').animate({width: '90%'}, 1500);
